5ec1cfdb3336d2613c8bd7a866a08210a3d3eaf6
codejredu <146084539+codejredu@users.noreply.github.com>
2024-06-17 08:13:20 +0300

    Add files via upload

--- scratchjr/index2.js
+++ scratchjr/index2.js
@@ -12615,6 +12615,7 @@ googleFormBtn.onclick = function() {
 googleFormBtn.innerText = "כתוב אלינו";
 
 // הגדרת סגנון הכפתור
+googleFormBtn.style.borderRadius = "10px"; // עיגול פינות הכפתור
 googleFormBtn.style.border = "2px solid #795548"; // גבול חום כהה
 googleFormBtn.style.backgroundColor = "#F0E68C"; // רקע צהוב בהיר
 googleFormBtn.style.color = "#333"; // צבע טקסט שחור
